3e961f674ce722e22440b9692ab05fd34ac329f2
guox <guox@goodrain.com>
2019-08-17 09:48:51 +0800

    upgrade

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM golang:1.12.9-alpine
 WORKDIR /mm_wiki
 ENV MM_HOME /mm_wiki
 ADD mm-wiki.conf ${MM_HOME}/mm-wiki.conf.template
-ADD https://github.com/phachon/mm-wiki/releases/download/v0.1.3/mm-wiki-linux-amd64.tar.gz /tmp/mm-wiki-linux-amd64.tar.gz
+ADD https://github.com/phachon/mm-wiki/releases/download/v0.1.3/mm-wiki-linux-amd64.tar.gz /mm_wiki/
 RUN echo "http://mirrors.aliyun.com/alpine/v3.4/main/" > /etc/apk/repositories && \
-    apk add --no-cache bash curl && \
-    tar xvzf /tmp/mm-wiki-linux-amd64.tar.gz -C /mm_wiki
+    apk add --no-cache bash
+    # tar xvzf /tmp/mm-wiki-linux-amd64.tar.gz -C /mm_wiki && \
+    # rm -rf /tmp/mm-wiki-linux-amd64.tar.gz
 
 COPY start.sh ${MM_HOME}/start.sh
 EXPOSE 8090
--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 [[ $DEBUG ]] && set -x
 
-if [ ! -f ${MM_HOME}/conf/template.conf ];then
-cp -a ${MM_HOME}/mm-wiki.conf.template ${MM_HOME}/conf/template.conf
+if [ ! -f ${MM_HOME}/conf/mm-wiki.conf ];then
+cp -a ${MM_HOME}/mm-wiki.conf.template ${MM_HOME}/conf/mm-wiki.conf
 fi
 
-if [ ! -f ${MM_HOME}/conf/inited ];then
-nohup ./install/install > ${MM_HOME}/conf/inited 2>&1 &
-fi
+# if [ ! -f ${MM_HOME}/conf/inited ];then
+# nohup ./install/install > ${MM_HOME}/conf/inited 2>&1 &
+# fi
 
-while true
-do
-grep "make conf file success" ${MM_HOME}/conf/inited
-if [ $? == 0 ];then
-break
-fi
-done
+# while true
+# do
+# grep "make conf file success" ${MM_HOME}/conf/inited
+# if [ $? == 0 ];then
+# break
+# fi
+# done
 
 [[ $PAUSE ]] && sleep $PAUSE
 ./mm-wiki --conf conf/mm-wiki.conf
